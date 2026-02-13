81d3583be84125decb2bb9f70a61638dcc35d6c9
Valeria Usuga <usugapenagosvaleria@gmail.com>
2026-02-13 13:29:59 -0500

    feat(App.jsx) Modificación del JS y el CSS

--- agenda-adso/src/App.jsx
+++ agenda-adso/src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default function App(){ const fecha = new Date().toLocaleString(); return (
+  <main>
+    <h1>Hola soy Valeria aprendiz de ADSO en el SENA</h1>
+    <p>
+      Espero aprender las bases de React para hacer más interactivos mis proyectos.
+    </p>
+  </main>
+)}

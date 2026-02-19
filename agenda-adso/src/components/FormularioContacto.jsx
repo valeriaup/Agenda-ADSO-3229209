0edff77c0b892942a6a7af118535e6e67f82bb8d
Valeria Usuga <usugapenagosvaleria@gmail.com>
2026-02-18 21:14:45 -0500

    feat(FormularioContacto.jsx) Clase_5_Agenda_ADSO_v3_LocalStorage

--- agenda-adso/src/components/FormularioContacto.jsx
+++ agenda-adso/src/components/FormularioContacto.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-
+//Clase 4
+/*import { useState } from "react";
 export default function FormularioContacto({ onAgregar }) {
   const [form, setForm] = useState({
     nombre: "",
@@ -61,6 +61,79 @@ export default function FormularioContacto({ onAgregar }) {
       />
 
       <button type="submit">Agregar contacto</button>
+    </form>
+  );
+}*/
+
+
+//Clase 5
+import { useState } from "react";
+
+export default function FormularioContacto({ onAgregar }) {
+  const [form, setForm] = useState({
+    nombre: "",
+    telefono: "",
+    correo: "",
+    etiqueta: "",
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setForm((f) => ({ ...f, [name]: value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!form.nombre || !form.telefono || !form.correo) return;
+
+    onAgregar(form);
+
+    setForm({
+      nombre: "",
+      telefono: "",
+      correo: "",
+      etiqueta: "",
+    });
+  };
+
+  return (
+    <form className="form-contacto" onSubmit={onSubmit}>
+      <label>Nombre *</label>
+      <input
+        name="nombre"
+        value={form.nombre}
+        onChange={onChange}
+        placeholder="Ej: Ana López"
+      />
+
+      <label>Teléfono *</label>
+      <input
+        name="telefono"
+        value={form.telefono}
+        onChange={onChange}
+        placeholder="Ej: 300 123 4567"
+      />
+
+      <label>Correo *</label>
+      <input
+        name="correo"
+        value={form.correo}
+        onChange={onChange}
+        placeholder="Ej: ana@sena.edu.co"
+      />
+
+      <label>Etiqueta (opcional)</label>
+      <input
+        name="etiqueta"
+        value={form.etiqueta}
+        onChange={onChange}
+        placeholder="Ej: Trabajo"
+      />
+
+      <button className="btn-primario" type="submit">
+        Agregar contacto
+      </button>
     </form>
   );
 }
